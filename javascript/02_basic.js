fff8707912120b9633fbba83a58dbc44a16ee318
Shlokthosar <shlok.2406216101@vcet.edu.in>
2026-07-28 11:58:13 +0000

    Adding basic JS files

---
+++ javascript/02_basic.js
@@ -0,0 +1,13 @@
+
+//let state= undefined nothing given
+let age=292
+
+//primitive data types
+// Different data types
+//string
+//number-2 power 53
+//Bolean true and false
+//null
+//undefined
+//symbol-unique
+console.log(typeof age);
