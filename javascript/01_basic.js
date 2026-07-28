1d5d085fecc0f74bd99df3a75db5bb0467f1929f
Shlokthosar <shlok.2406216101@vcet.edu.in>
2026-07-28 03:23:12 +0000

    Updated 01_basic.js

--- javascript/01_basic.js
+++ javascript/01_basic.js
@@ -3,4 +3,8 @@ let accountEmail="shlok@thos.com"
 var accountPassword="12455"
 var accountCity="Jaipur"
 
-console.log(accountId);
+console.log(accountId); 
+accountEmail="Shlok@1234"
+accountPassword="123566"
+accountCity="Mumbai"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
