686393a99994211910976ace1066f2207b9a3360
Shlokthosar <shlok.2406216101@vcet.edu.in>
2026-07-28 14:40:37 +0000

    Added 03_basic.js

---
+++ javascript/03_basic.js
@@ -0,0 +1,10 @@
+//let score=132
+
+//console.log(typeof(score));
+ //let Number= Number(score)
+ //console.log(typeof Number);
+ //console.log(Number);
+
+ let loggedIn=3
+  let apple=Boolean(loggedIn)
+  console.log(apple);
