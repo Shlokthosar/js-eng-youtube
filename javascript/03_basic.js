e21e130c58d9de8bb53f4f5931a0e1bafb482434
Shlokthosar <shlok.2406216101@vcet.edu.in>
2026-07-28 18:15:47 +0000

    Updated 03_basic.js

--- javascript/03_basic.js
+++ javascript/03_basic.js
@@ -9,6 +9,24 @@
   //let apple=Boolean(loggedIn)
   //console.log(apple);
 
-let sNumber=23
-let banana=String(sNumber)
-console.log(banana);
+// let sNumber=23
+// // let banana=String(sNumber)
+// // console.log(banana);
+
+// let number=5
+// let result= -number
+// console.log(result);
+
+// let String="67"
+// let apple=-String
+// console.log(apple);
+
+// console.log(3%2);
+// let str1= "Hello"
+// let str2 = " Shlok"
+// let str3= str1+str2
+// console.log(str3);
+
+let value=100
+value++;
+console.log(value);
