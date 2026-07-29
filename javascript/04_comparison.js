4700242e192b268bc0f74f512c77624fb6184ac1
Shlokthosar <shlok.2406216101@vcet.edu.in>
2026-07-29 04:24:10 +0000

    Added 04_comparison.js

---
+++ javascript/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("03">2);
+
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null!=0);
+
+
+let myname= "Shlok"
+let result= myname
+console.log (result)
+
+
